7d1294950e26737f82f10d32127c4c99a8960632
taerakwon <taerakwon@gmail.com>
2017-03-10 18:39:27 -0500

    Updated Toggle Button as Font-Awesome icon

--- server/app.js
+++ server/app.js
@@ -32,7 +32,6 @@ db.on('error', console.error.bind(console, 'mongoose connection error:'));
 db.once('open', () => {
   console.log("Successfully connected to MongoDB");
 })
-
 // DEFINE ROUTES
 let index = require('./routes/index');
 let contacts = require('./routes/businessContact'); // routes for businessContact after logged in
@@ -42,8 +41,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /client
-//app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../client', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
